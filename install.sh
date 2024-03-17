4af6460f79522f4ecabe7a0032798188fadb0732
Maxim Marsakov <mmarsakov@infinidat.com>
2024-03-17 08:14:11 +0000

    IMX-1669 Self Hosted Compose manifest with install.sh

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+set -eE
+
+INSTALLED_VERSION=""
+BASEDIR=$(dirname "$0")
+
+source "$BASEDIR"/install/cli.sh
+source "$BASEDIR"/install/check-min-req.sh
+source "$BASEDIR"/install/init-data.sh
+
+dc="docker compose --env-file .env --env-file $ENV_USER"
+
+catch_dc_errors() {
+    echo ""
+    echo "================================================================="
+    echo ""
+    echo "  An error during InfiniMetrics installation has occurred. "
+    echo "  Please see an output of \`$dc logs\` "
+    echo "  command."
+    echo ""
+    echo "================================================================="
+    echo ""
+}
+
+# Pull newest image version
+$dc pull
+
+# turn everything off 
+echo "INFO: Installation is in progress, please don't interrupt the procedure..."
+$dc down --remove-orphans --rmi local --timeout 120
+
+trap 'catch_dc_errors' ERR
+
+# build local docker images
+$dc build postgres
+echo "INFO: Docker images built."
+
+if [ ! "$SKIP_INIT" == "1" ]; then
+    echo "INFO: Starting web container to run database migrations..."
+    set -x
+    $dc run --rm web infinimetrics setup
+    INSTALLED_VERSION=$($dc run --rm web infinimetrics --version)
+    set +x
+fi
+
+if [ "$START_CONTAINERS" == "1" ]; then
+    # start all the other services
+    echo "INFO: Starting all services inside the compose..."
+    $dc up -d --wait
+else
+    echo ""
+    echo "================================================================="
+    echo ""
+    echo "  SUCCESS: InfiniMetrics installation has been completed."
+    echo "  InfiniMetrics installed version is $INSTALLED_VERSION"
+    echo "  Run the following command to get InfiniMetrics running:"
+    echo ""
+    echo "  $dc up -d"
+    echo ""
+    echo "=================================================================="
+    echo ""
+fi
+
---
+++ install/check-min-req.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+echo "INFO: Checking minimum requirements ..."
+
+MIN_DOCKER_VERSION='20.10.18'
+MIN_COMPOSE_VERSION='2.17.0'
+
+vercomp() {
+    printf "%s\n%s" "$1" "$2" | sort --version-sort --check=quiet --reverse
+    echo $?
+}
+
+DOCKER_VERSION=$(docker version --format '{{.Server.Version}}' || echo '')
+if [[ -z "$DOCKER_VERSION" ]]; then
+    echo "FAIL: Unable to get docker version, is the docker daemon running?"
+    exit 1
+fi
+
+if [[ "$(vercomp ${DOCKER_VERSION//v/} $MIN_DOCKER_VERSION)" -eq 1 ]]; then
+    echo "FAIL: The minimum supported docker version is $MIN_DOCKER_VERSION but found $DOCKER_VERSION"
+    exit 1
+fi
+echo "INFO: Docker version is $DOCKER_VERSION"
+
+DOCKER_ARCH=$(docker info --format '{{.Architecture}}')
+if [[ -z "$IMX_DEBUG" && ! "$DOCKER_ARCH" = "x86_64" ]]; then
+    echo "FAIL: Supported docker architecture is x86_64 but found $DOCKER_ARCH"
+    exit 1
+fi
+
+COMPOSE_VERSION=$(docker compose version --short || echo '')
+if [[ -z "$COMPOSE_VERSION" ]]; then
+    echo "FAIL: Docker compose is required"
+    exit 1
+fi
+
+if [[ "$(vercomp ${COMPOSE_VERSION//v/} $MIN_COMPOSE_VERSION)" -eq 1 ]]; then
+    echo "FAIL: The minimum supported docker compose version is $MIN_COMPOSE_VERSION but found $COMPOSE_VERSION"
+    exit 1
+fi
+echo "INFO: Found Docker Compose version $COMPOSE_VERSION"
+
