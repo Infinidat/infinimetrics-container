0d7392962976796250fb950e098a29475455fa48
Maxim Marsakov <mmarsakov@infinidat.com>
2024-04-16 13:44:49 +0300

    TRIVIAL change run to exec to reload gunicorn on restore

--- infinimetrics.sh
+++ infinimetrics.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-exec docker compose --env-file .env --env-file .env.user run --rm -i web infinimetrics "$@"
+exec docker compose --env-file .env --env-file .env.user exec web infinimetrics "$@"
